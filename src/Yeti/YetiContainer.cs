dd942b935edb1625ba9f1faf5b3ead800dbfb45d
Ariel R <rzeszowsky@gmail.com>
2020-03-23 15:31:11 +0100

    Added support for components with dependecies

--- src/Yeti/YetiContainer.cs
+++ src/Yeti/YetiContainer.cs
@@ -21,6 +21,38 @@ namespace Yeti
 
             Type actual_type = _registrations[requested_type];
 
+            var ctors = actual_type.GetConstructors();
+
+            var ctor = ctors.First();
+
+            IEnumerable<Type> dependency_types = ctor.GetParameters()
+                .Select(x => x.ParameterType);
+
+
+
+            var instance = Activator.CreateInstance(actual_type);
+
+            return (T)instance;
+        }
+
+        object Resolve(Type type)
+        {
+            var requested_type = type;
+
+            Type actual_type = _registrations[requested_type];
+
+            var ctors = actual_type.GetConstructors();
+
+            var ctor = ctors.First();
+
+            IEnumerable<Type> dependency_types = ctor.GetParameters()
+                .Select(x => x.ParameterType);
+
+            var dependencies = dependency_types
+                .Select(x => this.Resolve(x))
+                .ToArray();
+
+
             var instance = Activator.CreateInstance(actual_type);
 
             return (T)instance;
--- tests/YeTi.Tests/yeti_container_tests.cs
+++ tests/YeTi.Tests/yeti_container_tests.cs
@@ -20,16 +20,39 @@ namespace YeTi.Tests
             var resolved_object = container.Resolve<ITestInterface>();
 
             resolved_object.ShouldBeOfType<TestImplementation>();
+        }
 
-        } 
+        [Fact]
+        public void resolves_components_with_ctor_with_dependencies()
+        {
+            var container = new YetiContainer();
+            container.Register<Dependecy, Dependecy>();
+            container.Register<ITestInterface, TestImplementationWithDepenedncy>();
+
+            var resolved_object = container.Resolve<ITestInterface>();
+
+            resolved_object.ShouldBeOfType<TestImplementationWithDepenedncy>();
+        }
 
         public interface ITestInterface
         {
-
         }
 
         public class TestImplementation : ITestInterface
         {
+        }
+
+        public class Dependecy
+        {
+        }
+
+
+        public class TestImplementationWithDepenedncy : ITestInterface
+        {
+            public TestImplementationWithDepenedncy(Dependecy dependecy)
+            {
+
+            }
 
         }
     }
