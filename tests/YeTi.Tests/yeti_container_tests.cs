9f03cd03fe3cd1c25a3bd134dec392f94a7029a4
Ariel R <rzeszowsky@gmail.com>
2020-03-23 13:20:00 +0100

    Added test for register & resolve operation

---
+++ tests/YeTi.Tests/yeti_container_tests.cs
@@ -0,0 +1,39 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Yeti;
+
+namespace YeTi.Tests
+{
+    public class yeti_container_tests
+    {
+        [Fact]
+        public void resolves_registered_components()
+        {
+            // Arange
+            var container = new YetiContainer();
+            container.Register<ITestInterface, TestImplementation>();
+
+            //Act
+            var resolved_object = container.Resolve<ITestInterface>();
+
+            // Assert 
+            resolved_object.ShouldBeOfType<TestImplementation>();
+
+        } 
+
+        public interface ITestInterface
+        {
+
+        }
+
+        public class TestImplementation : ITestInterface
+        {
+
+        }
+    }
+}
