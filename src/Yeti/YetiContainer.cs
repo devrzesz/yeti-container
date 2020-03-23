4faff7bd286f4dc9452da38cde5de567ed3d6a2f
Ariel R <rzeszowsky@gmail.com>
2020-03-23 13:20:55 +0100

    implemented basic operation in YetiContainer

---
+++ src/Yeti/YetiContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yeti
+{
+    public class YetiContainer
+    {
+        readonly Dictionary<Type, Type> _registrations = new Dictionary<Type, Type>();
+
+        public void Register<TRegistration, TImplementation>()
+        {
+            _registrations.Add(typeof(TRegistration), typeof(TImplementation));
+        }
+
+        public T Resolve<T>()
+        {
+            var requested_type = typeof(T);
+
+            Type actual_type = _registrations[requested_type];
+
+            var instance = Activator.CreateInstance(actual_type);
+
+            return (T)instance;
+        }
+    }
+}
